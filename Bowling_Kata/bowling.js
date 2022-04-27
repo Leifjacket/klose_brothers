4482b465e367c1ef60cab42de4cd8738ab57b4ad
Lukas Filges <Lukas.Filges@gmx.net>
2022-04-27 16:44:33 +0200

    Mostly Correct solution, one edge case missing with a spare and the last frame, still need lots of tests

--- Bowling_Kata/bowling.js
+++ Bowling_Kata/bowling.js
@@ -23,21 +23,43 @@ class BowlingCalculator {
 
     calculateScore(){
         //Calculates the Overall score and get returned
-        var overallScore=0;
+        var overallScore = 0;
 
         //When a Strike in frames 1-9 got thrown this is true, otherwise false
-        var oddNumberOfThrows=false;
-        var bonusThrow = this.bonusThrow();
-        for(var counter=0;counter<this.throws.length;counter++){
-            //calculate value of a Spare in a frame
-            // if(this.throws[counter] % 2 == 0 && !oddNumberOfThrows){
+        var oddStrike = false;
+        var lastThrows = 2;
+        if(this.bonusThrow()){
+            lastThrows = 3;
+        }
+        for(var roll = 0; roll < this.throws.length;roll++){
 
-            // } 
-            
-            //TODO: calculate value of Strike in a frame
-
-            //normal points added for the frame
-            overallScore = overallScore + this.throws[counter];
+            //calculation for frame 1-9
+            if(roll < this.throws.length-lastThrows) {
+                //is it a spare, strike, or not cleared?
+                if(this.throws[roll] + this.throws[roll+1] == 10 && !oddStrike){
+                    var spareValue = 10 + this.throws[roll+2];
+                    overallScore = overallScore + spareValue;
+                    //increase roll because we already counted the next roll aswell for the spare
+                    roll++;
+                } else if(this.throws[roll]==10){
+                    var strikeValue = 10 + this.throws[roll+1] + this.throws[roll+2];
+                    overallScore = overallScore + strikeValue;
+                    // reverse oddStrike Boolean, important for calculating a Spare, for reference when 2 rolls add up to 10 points and it can be either the start of a frame or 2 different frames 
+                    oddStrike = !oddStrike;
+                } else {
+                    //normal points added for the frame
+                    overallScore = overallScore + this.throws[roll]+this.throws[roll+1];
+                    //increase roll because we already counted the next roll aswell
+                    roll++;
+                }
+            } else {
+                //calculation for frame 10
+                overallScore = overallScore + this.throws[this.throws.length-2] + this.throws[this.throws.length-1];
+                if(this.bonusThrow()){
+                    overallScore = overallScore + this.throws[this.throws.length-2];
+                }
+                roll = roll + 3;
+            }
         }
         return overallScore;
     }
